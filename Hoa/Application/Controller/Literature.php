ba2285dbf74da1060a2a6d9ff0d56b42480ead7d
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-03-16 15:58:02 +0100

    Add Literature.html and Video.html

--- Hoa/Application/Controller/Literature.php
+++ Hoa/Application/Controller/Literature.php
@@ -2,6 +2,14 @@
 
 class LiteratureController extends \Hoa\Controller\Application {
 
+    public function DefaultAction ( )  {
+
+        $this->view->addOverlay('hoa://Application/View/Literature/Literature.xyl');
+        $this->view->render();
+
+        return;
+    }
+
     public function LearnAction ( )  {
 
         $this->view->addOverlay('hoa://Application/View/Literature/Learn.xyl');
--- Hoa/Application/Controller/Video.php
+++ Hoa/Application/Controller/Video.php
@@ -2,6 +2,14 @@
 
 class VideoController extends \Hoa\Controller\Application {
 
+    public function DefaultAction ( ) {
+
+        $this->view->addOverlay('hoa://Application/View/Video/Video.xyl');
+        $this->view->render();
+
+        return;
+    }
+
     public function PraspelAction ( )  {
 
         $this->view->addOverlay('hoa://Application/View/Video/Praspel.xyl');
